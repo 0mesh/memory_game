b0f2ee0f8ae0ab2a6ec0bd42ba93d13ad11afe59
0mesh <0mesh@users.github.com>
2020-08-08 23:49:11 +0530

    Updates

---
+++ precache-manifest.718c54d1a8c780b8df9131309d405d6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92565200600bf21fa4890265ad23d57f",
+    "url": "/memory_game/index.html"
+  },
+  {
+    "revision": "513775d62695c2164931",
+    "url": "/memory_game/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "59bf66c721948d76de13",
+    "url": "/memory_game/static/css/main.f62e466f.chunk.css"
+  },
+  {
+    "revision": "513775d62695c2164931",
+    "url": "/memory_game/static/js/2.8119c69e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/memory_game/static/js/2.8119c69e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59bf66c721948d76de13",
+    "url": "/memory_game/static/js/main.6d6a7aad.chunk.js"
+  },
+  {
+    "revision": "f6d0efd7b9ba1f2f2ba2",
+    "url": "/memory_game/static/js/runtime-main.e5e5641f.js"
+  }
+]);
